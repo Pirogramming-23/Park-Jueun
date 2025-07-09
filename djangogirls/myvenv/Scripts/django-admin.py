384ae994f02bc946b25a7893299e686738ecfd60
ParkJueun <jueun036@gmail.com>
2025-07-09 16:30:00 +0900

    djangogirls-설치하기 학습완료

---
+++ djangogirls/myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\jueun\Desktop\PIRO_23_Assignment\Park-Jueun\djangogirls\myvenv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
